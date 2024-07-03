7b516ee8f8466a9b01b43c6daa1442998e8aa468
jmindeveloper <jmin.developer@gmail.com>
2024-07-03 22:35:15 +0900

    [feat] collectionView constraints 설정 #8

--- ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/SearchCharacterViewController.swift
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/SearchCharacterViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchCharacterViewController: UIViewController {
+    private let searchCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navigationItem.title = "Search"
+        setSubViews()
     }
     
+    private func setSubViews() {
+        [searchCollectionView].forEach {
+            view.addSubview($0)
+        }
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        searchCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
