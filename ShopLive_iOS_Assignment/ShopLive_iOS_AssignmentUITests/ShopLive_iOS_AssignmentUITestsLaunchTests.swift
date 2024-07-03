7c09b6d610fd3eede67863c9e320512548d1aa61
jmindeveloper <jmin.developer@gmail.com>
2024-07-03 20:51:06 +0900

    [seetting] xcode project 생성 #1

---
+++ ShopLive_iOS_Assignment/ShopLive_iOS_AssignmentUITests/ShopLive_iOS_AssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShopLive_iOS_AssignmentUITestsLaunchTests.swift
+//  ShopLive_iOS_AssignmentUITests
+//
+//  Created by J_Min on 7/3/24.
+//
+
+import XCTest
+
+final class ShopLive_iOS_AssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
