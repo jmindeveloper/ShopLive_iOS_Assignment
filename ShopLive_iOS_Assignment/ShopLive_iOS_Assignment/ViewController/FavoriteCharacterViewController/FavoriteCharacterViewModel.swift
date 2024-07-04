2bf1792f63d84683083cf1262731d2544e0e779b
jmindeveloper <jmin.developer@gmail.com>
2024-07-04 14:23:38 +0900

    [feat] FavoriteCharacterVM 생성 #29

---
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController/FavoriteCharacterViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteCharacterViewModel.swift
+//  ShopLive_iOS_Assignment
+//
+//  Created by J_Min on 7/4/24.
+//
+
+import Foundation
+import Combine
+
+final class FavoriteCharacterViewModel {
+    
+    // MARK: - Properties
+    private let coreDataManager = CoreDataManager()
+}
