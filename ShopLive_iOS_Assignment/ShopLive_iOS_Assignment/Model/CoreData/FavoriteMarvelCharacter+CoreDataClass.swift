f6d9d0bfc7e9a56a335d02ea106561a8c43e9fca
jmindeveloper <jmin.developer@gmail.com>
2024-07-04 12:03:18 +0900

    [feat] Favorite Character coredata model 생성 #19

---
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/Model/CoreData/FavoriteMarvelCharacter+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteMarvelCharacter+CoreDataClass.swift
+//  ShopLive_iOS_Assignment
+//
+//  Created by J_Min on 7/4/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavoriteMarvelCharacter)
+public class FavoriteMarvelCharacter: NSManagedObject {
+
+}
