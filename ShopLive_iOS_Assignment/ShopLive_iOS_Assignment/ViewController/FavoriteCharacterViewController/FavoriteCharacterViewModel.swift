fb15d8770ba9eb3844117becb4edb1138f83a113
jmindeveloper <jmin.developer@gmail.com>
2024-07-04 15:02:27 +0900

    [feat] FavoriteCharacterVM 구현 #29

--- ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController/FavoriteCharacterViewModel.swift
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController/FavoriteCharacterViewModel.swift
@@ -8,8 +8,41 @@
 import Foundation
 import Combine
 
-final class FavoriteCharacterViewModel {
+protocol FavoriteCharacterViewModelProtocol {
+    var favoriteMarvelCharacters: [FavoriteMarvelCharacter] { get set }
+    var collectionViewUpdatePublisher: PassthroughSubject<Void, Never> { get }
+    
+    init(coreDataManager: CoreDataManagerProtocol)
+    
+    func deleteFavoriteMarvelCharacter(index: Int)
+}
+
+final class FavoriteCharacterViewModel: FavoriteCharacterViewModelProtocol {
     
     // MARK: - Properties
-    private let coreDataManager = CoreDataManager()
+    private let coreDataManager: CoreDataManagerProtocol
+    var favoriteMarvelCharacters: [FavoriteMarvelCharacter] = [] {
+        didSet {
+            collectionViewUpdatePublisher.send()
+        }
+    }
+    let collectionViewUpdatePublisher = PassthroughSubject<Void, Never>()
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init(coreDataManager: CoreDataManagerProtocol) {
+        self.coreDataManager = coreDataManager
+        binding()
+    }
+    
+    private func binding() {
+        coreDataManager.favoriteCharacterPublisher
+            .sink { [weak self] character in
+                self?.favoriteMarvelCharacters = character
+            }.store(in: &subscriptions)
+    }
+    
+    func deleteFavoriteMarvelCharacter(index: Int) {
+        let character = favoriteMarvelCharacters[index]
+        coreDataManager.deleteFavoriteCharacter(character: character)
+    }
 }
