491ca9e3dde6071f8b4a251c736689c4d6308aac
jmindeveloper <jmin.developer@gmail.com>
2024-07-04 19:24:59 +0900

    [test] getMarvelCharacter success, failure 테스트 #40

--- ShopLive_iOS_Assignment/ShopLive_iOS_AssignmentTests/ShopLive_iOS_Assignment_NetworkManagerTests.swift
+++ ShopLive_iOS_Assignment/ShopLive_iOS_AssignmentTests/ShopLive_iOS_Assignment_NetworkManagerTests.swift
@@ -8,31 +8,113 @@
 import Foundation
 
 import XCTest
+import Combine
 @testable import ShopLive_iOS_Assignment
 
 final class ShopLive_iOS_Assignment_NetworkManagerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var networkManager: NetworkManager!
+    var mockSession: MockURLSession!
+    private var subscriptions = Set<AnyCancellable>()
+    
+    override func setUp() {
+        super.setUp()
+        mockSession = MockURLSession()
+        networkManager = NetworkManager(session: mockSession)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        networkManager = nil
+        mockSession = nil
+        subscriptions.removeAll()
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_getMarvelCharactersSuccess() async throws {
+        // Given
+        let jsonString = """
+            {
+                "data": {
+                    "count": 1,
+                    "results": [
+                        {
+                            "id": 1017100,
+                            "name": "아이언맨",
+                            "description": "블루스컬",
+                            "thumbnail": {
+                                "path": "http://example.com",
+                                "extension": "jpg"
+                            }
+                        }
+                    ]
+                }
+            }
+            """
+        mockSession.data = jsonString.data(using: .utf8)
+        mockSession.response = HTTPURLResponse(
+            url: URL(string: "https://example.com")!,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )
+        
+        let resource = Resource(
+            base: "https://example.com",
+            path: "",
+            params: [:]
+        )
+        
+        // When
+        do {
+            try networkManager.getMarvelCharacters(resource: resource)
+        } catch {
+            XCTFail()
+        }
+        
+        Thread.sleep(forTimeInterval: 0.5)
+        
+        // Then
+        networkManager.characterPublisher
+            .sink { characters in
+                XCTAssertEqual(characters.count, 1)
+                XCTAssertEqual(characters.first?.name, "아이언맨")
+            }.store(in: &subscriptions)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_getMarvelCharactersFailure_badServerResponse() async throws {
+        // Given
+        mockSession.data = nil
+        
+        let resource = Resource(
+            base: "https://example.com",
+            path: "",
+            params: [:]
+        )
+        
+        // When
+        do {
+            try networkManager.getMarvelCharacters(resource: resource)
+        } catch {
+            // Then
+            XCTAssertEqual(error as? URLError, URLError(.badServerResponse))
         }
     }
-
+    
+    func test_getMarvelCharactersFailure_badURL() async throws {
+        // Given
+        mockSession.error = URLError(.badURL)
+        
+        let resource = Resource(
+            base: "https://example.com",
+            path: "",
+            params: [:]
+        )
+        
+        // When
+        do {
+            try networkManager.getMarvelCharacters(resource: resource)
+        } catch {
+            // Then
+            XCTAssertEqual(error as? URLError, URLError(.badURL))
+        }
+    }
 }
