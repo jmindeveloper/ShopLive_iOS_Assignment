7e7532c2effef491a17abccf4ce60f10f9443caa
jmindeveloper <jmin.developer@gmail.com>
2024-07-04 14:12:59 +0900

    [feat] favoriteVC collectionView cell 설정 #27

--- ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController.swift
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController.swift
@@ -17,8 +17,8 @@ final class FavoriteCharacterViewController: UIViewController {
             forCellWithReuseIdentifier: CharacterCardCollectionViewCell.identifier
         )
         
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
         
         return collectionView
     }()
@@ -26,8 +26,16 @@ final class FavoriteCharacterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navigationItem.title = "Favorite"
+        setSubViews()
+        binding()
     }
     
+    private func binding() {
+        
+    }
+    
+    // MARK: - setSubViews
     private func setSubViews() {
         [searchCollectionView].forEach {
             view.addSubview($0)
@@ -44,7 +52,7 @@ final class FavoriteCharacterViewController: UIViewController {
     
     // MARK: - CompositionalLayout
     private func favoriteCollectionViewLayout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1.0))
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = .init(top: 0, leading: 3, bottom: 10, trailing: 3)
         
@@ -58,5 +66,33 @@ final class FavoriteCharacterViewController: UIViewController {
         
         return layout
     }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+extension FavoriteCharacterViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return characterMockData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: CharacterCardCollectionViewCell.identifier,
+            for: indexPath
+        ) as? CharacterCardCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configureView(
+            model: characterMockData[indexPath.row],
+            isFavorite: false
+        )
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension FavoriteCharacterViewController: UICollectionViewDelegate {
     
 }
