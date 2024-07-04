5f8b637a9b814ced6dd505f3938513ee26bfae3c
jmindeveloper <jmin.developer@gmail.com>
2024-07-04 14:05:15 +0900

    [feat] favoriteVC collectionView constraints 및 layout 설정 #27

--- ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController.swift
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/ViewController/FavoriteCharacterViewController.swift
@@ -9,9 +9,54 @@ import UIKit
 
 final class FavoriteCharacterViewController: UIViewController {
     
+    // MARK: - ViewProperties
+    private lazy var searchCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: favoriteCollectionViewLayout())
+        collectionView.register(
+            CharacterCardCollectionViewCell.self,
+            forCellWithReuseIdentifier: CharacterCardCollectionViewCell.identifier
+        )
+        
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+    }
+    
+    private func setSubViews() {
+        [searchCollectionView].forEach {
+            view.addSubview($0)
+        }
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        searchCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - CompositionalLayout
+    private func favoriteCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 3, bottom: 10, trailing: 3)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
     }
     
 }
