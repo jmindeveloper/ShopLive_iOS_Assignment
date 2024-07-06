c4dd2bd41fd854ebaa3218fd5590075c03362b80
jmindeveloper <jmin.developer@gmail.com>
2024-07-06 17:08:37 +0900

    [feat] AlertManager 구현 #47

---
+++ ShopLive_iOS_Assignment/ShopLive_iOS_Assignment/Extension/UIApplication+.swift
@@ -0,0 +1,25 @@
+//
+//  UIApplication+.swift
+//  ShopLive_iOS_Assignment
+//
+//  Created by J_Min on 7/6/24.
+//
+
+import UIKit
+
+extension UIApplication {
+    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+}
